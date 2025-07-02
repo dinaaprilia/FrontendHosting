ef12d69082dfc2ebdf788c6775be82bb97255253
dinaaprilia <132429093+dinaaprilia@users.noreply.github.com>
2025-07-02 10:54:24 +0800

    Update FormKehadiran.jsx

--- app/(dashboard)/ekskul/[name]/_components/FormKehadiran.jsx
+++ app/(dashboard)/ekskul/[name]/_components/FormKehadiran.jsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import { FaExternalLinkAlt } from "react-icons/fa";
 
-const API_BASE = "http://localhost:8000/api";
+const API_BASE = "https://backendfix-production.up.railway.app/api";
 
 export default function KehadiranEkskul({ ekskulId, ekskulName }) {
   const [students, setStudents] = useState([]);
