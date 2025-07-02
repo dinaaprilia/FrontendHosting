8acfa6cedd5b802e7637f8ebbcbbf76fbc6a157f
dinaaprilia <132429093+dinaaprilia@users.noreply.github.com>
2025-07-02 10:31:13 +0800

    Update AktivitasTerkini.jsx

--- app/(dashboard)/beranda/_components/AktivitasTerkini.jsx
+++ app/(dashboard)/beranda/_components/AktivitasTerkini.jsx
@@ -14,7 +14,7 @@ const RecentActivity = () => {
   useEffect(() => {
     const fetchActivities = async () => {
       try {
-        const response = await fetch("http://localhost:8000/api/recent-activity", {
+        const response = await fetch("https://backendfix-production.up.railway.app/api/recent-activity", {
           headers: {
             "Content-Type": "application/json",
             // Jika endpoint dilindungi oleh JWT:
