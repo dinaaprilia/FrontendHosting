2c8a1c5dade15567ec282f7773dfe9df0a17172d
dinaapriliabrgt <apriliawowor54@gmail.com>
2025-07-02 14:46:00 +0800

    Update page.jsx

--- app/(dashboard)/detailabsensi/page.jsx
+++ app/(dashboard)/detailabsensi/page.jsx
@@ -1,42 +1,21 @@
-"use client"; // ✅ Tambahkan ini
-
-import { useSearchParams } from "next/navigation";
+import React, { Suspense } from "react";
 import Sidebar from "@/app/_components/Sidebar";
 import Header from "@/app/_components/Header";
-import { FaArrowLeft } from "react-icons/fa";
-import StudentCard from "./_components/DetailSiswa";
-import AttendanceTable from "./_components/RiwayatBulanan";
+import DetailAbsensiClient from "./DetailAbsensiClient";
 
-const DetailAbsensi = () => {
-  const searchParams = useSearchParams(); // ✅ Ambil query dari URL
-    return (
-      
-      <div className="flex h-screen">
-        {/* Header */}
-        <Header className="fixed top-0 left-0 w-full bg-white z-50 shadow-md" />
-  
-        {/* Sidebar */}
-        <Sidebar />
-  
-  
-        {/* Konten Utama */}
-        <main className="flex-1 bg-gray-200 p-4 flex flex-col items-center justify-start w-full overflow-y-auto ">
-        <h1 className="sm:text-3xl text-2xl text-black font-bold w-full max-w-6xl sm:mt-14 mt-10 -mb-3 flex items-center justify-between">
-    Detail Kehadiran Siswa
-    <button 
-      onClick={() => window.history.back()} 
-      className="flex items-center gap-2 text-gray-700 hover:text-gray-900 transition duration-300 mr-auto ml-2"
-    >
-      <FaArrowLeft className="text-1xl " />
-    </button>
-  </h1>
+export default function DetailAbsensiPage() {
+  return (
+    <div className="flex h-screen">
+      {/* Header */}
+      <Header className="fixed top-0 left-0 w-full bg-white z-50 shadow-md" />
 
-  <StudentCard/>
+      {/* Sidebar */}
+      <Sidebar />
 
-  <AttendanceTable/>
-        </main>
-      </div>
-    );
-  }
-  
-export default DetailAbsensi;
+      {/* Client Component dibungkus Suspense */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <DetailAbsensiClient />
+      </Suspense>
+    </div>
+  );
+}
