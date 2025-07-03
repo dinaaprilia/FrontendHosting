998c7ab5ef48da2f363696973343e2ea8f4b457c
dinaapriliabrgt <apriliawowor54@gmail.com>
2025-07-03 23:00:04 +0800

    Update page.jsx

--- app/(dashboard)/Inputabsensi/page.jsx
+++ app/(dashboard)/Inputabsensi/page.jsx
@@ -1,51 +1,5 @@
-"use client"; 
-
-import React, { useEffect } from "react";
-import Sidebar from "@/app/_components/Sidebar";
-import Header from "@/app/_components/Header";
-import { FaArrowLeft } from "react-icons/fa";
-import AttendanceForm from "./_components/AbsensiForm";
-import StudentList from "./_components/ListSiswa";
-import BackButton from "./_components/BackButton";
-import { useSearchParams } from "next/navigation";
+import AttendancePageClient from "./_components/AttendancePageClient";
 
 export default function InputAbsensiPage() {
-  const searchParams = useSearchParams();
-  const kelas = searchParams.get("kelas") || "Tidak Diketahui";
-
-  useEffect(() => {
-    // Hide global scroll when this page is mounted
-    document.body.style.overflow = "hidden";
-    document.documentElement.style.overflow = "hidden";
-
-    // Restore global scroll when leaving this page
-    return () => {
-      document.body.style.overflow = "";
-      document.documentElement.style.overflow = "";
-    };
-  }, []);
-
-  return (
-    <div className="flex h-screen overflow-hidden">
-      <Header className="fixed top-0 left-0 w-full bg-white z-50 shadow-md" />
-      <Sidebar />
-
-      <main className="flex-1 p-4 bg-gray-200 overflow-y-auto">
-        <div className="flex items-center justify-between sm:mt-14 mt-10 sm:mb-4 mb-2 max-w-6xl">
-          <h1 className="sm:text-3xl text-2xl text-black font-bold">
-            Input Absensi - {kelas}
-          </h1>
-          <BackButton />
-        </div>
-
-        <div className="mt-5">
-          <AttendanceForm />
-        </div>
-
-        <div className="mt-5">
-          <StudentList />
-        </div>
-      </main>
-    </div>
-  );
+  return <AttendancePageClient />;
 }
