f52dea51c2219f1f2105832aa5238826eac35d73
dinaapriliabrgt <apriliawowor54@gmail.com>
2025-07-04 00:36:18 +0800

    Update page.jsx

--- app/(dashboard)/Inputabsensi/page.jsx
+++ app/(dashboard)/Inputabsensi/page.jsx
@@ -1,7 +1,8 @@
+"use client";
+
 import React from "react";
 import Sidebar from "@/app/_components/Sidebar";
 import Header from "@/app/_components/Header";
-import { FaArrowLeft } from "react-icons/fa";
 import AttendanceForm from "./_components/AbsensiForm";
 import StudentList from "./_components/ListSiswa";
 import BackButton from "./_components/BackButton";
@@ -10,27 +11,33 @@ export default function InputAbsensiPage({ searchParams }) {
   const kelas = searchParams.kelas || "Tidak Diketahui";
 
   return (
-    <div className="flex h-screen overflow-hidden">
+    <div className="flex flex-col h-screen">
+      {/* Header */}
       <Header className="fixed top-0 left-0 w-full bg-white z-50 shadow-md" />
-      <Sidebar />
 
-      <main className="flex-1 p-4 bg-gray-200 overflow-y-auto">
-        <div className="flex items-center justify-between sm:mt-14 mt-10 sm:mb-4 mb-2 max-w-6xl">
-          <h1 className="sm:text-3xl text-2xl text-black font-bold">
-            Input Absensi - {kelas}
-          </h1>
-                    <BackButton />
+      {/* Content */}
+      <div className="flex flex-1 pt-16 overflow-hidden">
+        {/* Sidebar */}
+        <Sidebar />
 
-        </div>
+        {/* Main Content */}
+        <main className="flex-1 p-4 bg-gray-200 overflow-y-auto">
+          <div className="flex items-center justify-between sm:mt-0 mt-0 sm:mb-4 mb-2 max-w-6xl">
+            <h1 className="sm:text-3xl text-2xl text-black font-bold">
+              Input Absensi - {kelas}
+            </h1>
+            <BackButton />
+          </div>
 
-        <div className="mt-5">
-          <AttendanceForm />
-        </div>
+          <div className="mt-5">
+            <AttendanceForm />
+          </div>
 
-        <div className="mt-5">
-          <StudentList />
-        </div>
-      </main>
+          <div className="mt-5">
+            <StudentList />
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
